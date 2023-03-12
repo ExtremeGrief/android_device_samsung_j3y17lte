c7468fc6a65125f6135d9e3367d66203c3c3879d
ExtremeXT <ttoeosks@gmail.com>
2023-03-12 16:23:15 +0200

    Change fstab name

--- omni_j3y17lte.mk
+++ omni_j3y17lte.mk
@@ -3,6 +3,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := j3y17lte
 PRODUCT_NAME := omni_j3y17lte
-PRODUCT_MODEL := SM-J300X
+PRODUCT_MODEL := SM-J330
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
